f17dd2103f1a325e008956b5ee66ea8ce8196ad2
louis N <l.nonone@codeur.online>
2021-08-26 16:36:58 +0200

    ajout du Delete 🚨

---
+++ functions/deleteAnnonce.php
@@ -0,0 +1,12 @@
+<?php
+require_once(dirname(__FILE__) . "/../Model/database.php");
+
+
+//Supprime moi une annonce lorsque where id = a annonce id
+$req = $db->prepare("DELETE FROM annonce WHERE id = :annonce_id ");
+//Je selectionne la requete pr lui passer un parametre et sa valeur sera ce que l'on reçoit via l'url
+$req->bindParam(":annonce_id", $_GET["annonce_id"]);
+$req->execute();
+
+header("Location: ../index.php");
+exit();
